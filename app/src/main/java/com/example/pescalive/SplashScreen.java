dafc192b11091aa90659df00bc5d5469382e8b2b
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-29 21:01:13 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/pescalive/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.pescalive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Button splash_screen_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        splash_screen_button = findViewById(R.id.splash_screen_button);
+
+        splash_screen_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TournamentsListScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
