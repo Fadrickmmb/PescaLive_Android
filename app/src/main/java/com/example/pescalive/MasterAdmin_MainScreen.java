6fba27ab41e0aaeead8508cd963d4ddaf3c4057d
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-08-30 20:34:12 -0400

    MasterAdmin_MainScreen Redirection for other MasterAdmin screens

--- app/src/main/java/com/example/pescalive/MasterAdmin_MainScreen.java
+++ app/src/main/java/com/example/pescalive/MasterAdmin_MainScreen.java
@@ -1,6 +1,9 @@
 package com.example.pescalive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MasterAdmin_MainScreen extends AppCompatActivity {
+
+    Button masterAdmin_mainScreen_createTournament_button, masterAdmin_mainScreen_editTournament_button;
+    Button masterAdmin_mainScreen_createUser_button, masterAdmin_mainScreen_editUser_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,48 @@ public class MasterAdmin_MainScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        masterAdmin_mainScreen_createTournament_button = findViewById(R.id.masterAdmin_mainScreen_createTournament_button);
+        masterAdmin_mainScreen_editTournament_button = findViewById(R.id.masterAdmin_mainScreen_editTournament_button);
+        masterAdmin_mainScreen_createUser_button = findViewById(R.id.masterAdmin_mainScreen_createUser_button);
+        masterAdmin_mainScreen_editUser_button = findViewById(R.id.masterAdmin_mainScreen_editUser_button);
+
+        masterAdmin_mainScreen_createTournament_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MasterAdmin_CreateTournamentScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        masterAdmin_mainScreen_editTournament_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MasterAdmin_EditTournamentScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        masterAdmin_mainScreen_createUser_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MasterAdmin_CreateUserScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        masterAdmin_mainScreen_editUser_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MasterAdmin_EditUserScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
     }
 }
