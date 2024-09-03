5d039dffe6ffc6c0334107c7fb5d95c32595c0fe
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-09-02 22:16:03 -0400

    Working on the Edit Tournament Screen

--- app/src/main/java/com/example/pescalive/MasterAdmin_EditTournamentScreen.java
+++ app/src/main/java/com/example/pescalive/MasterAdmin_EditTournamentScreen.java
@@ -1,6 +1,12 @@
 package com.example.pescalive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MasterAdmin_EditTournamentScreen extends AppCompatActivity {
+
+    Spinner tournamentNameSpinner;
+    EditText changeNameInput;
+    NumberPicker addRoundPicker;
+    Spinner deleteRoundSpinner;
+
+    Button saveButton, cancelButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,22 @@ public class MasterAdmin_EditTournamentScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tournamentNameSpinner = findViewById(R.id.masterAdmin_editTournament_tournamentName_spinner);
+        changeNameInput = findViewById(R.id.masterAdmin_editTournament_changeName_input);
+        addRoundPicker = findViewById(R.id.masterAdmin_editTournament_addRound_picker);
+        deleteRoundSpinner = findViewById(R.id.masterAdmin_editTournament_deleteRound_spinner);
+        saveButton = findViewById(R.id.masterAdmin_editTournament_save_button);
+        cancelButton = findViewById(R.id.masterAdmin_editTournament_cancel_button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MasterAdmin_MainScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
