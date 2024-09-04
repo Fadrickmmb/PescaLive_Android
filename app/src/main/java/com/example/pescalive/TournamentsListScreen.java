8035ced5f5b8fc0332d047b1ab8ac067c2a05a5d
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-09-03 21:27:25 -0400

    Tournaments Fetched from the Database and Shown on TournamentsListScreen

--- app/src/main/java/com/example/pescalive/TournamentsListScreen.java
+++ app/src/main/java/com/example/pescalive/TournamentsListScreen.java
@@ -6,14 +6,30 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TournamentsListScreen extends AppCompatActivity {
 
     Button loginButton;
+    private RecyclerView tournamentsRecyclerView;
+    private TournamentAdapter tournamentAdapter;
+    private List<Tournament> tournamentList;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,14 @@ public class TournamentsListScreen extends AppCompatActivity {
         });
 
         loginButton = findViewById(R.id.tournamentsList_login_button);
+        tournamentsRecyclerView = findViewById(R.id.tournaments_recycler_view);
+        tournamentsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        tournamentList = new ArrayList<>();
+        tournamentAdapter = new TournamentAdapter(tournamentList, this);
+        tournamentsRecyclerView.setAdapter(tournamentAdapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("tournaments");
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +61,29 @@ public class TournamentsListScreen extends AppCompatActivity {
             }
         });
 
+        fetchTournaments();
     }
+
+    private void fetchTournaments() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                tournamentList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Tournament tournament = snapshot.getValue(Tournament.class);
+                    tournament.setTournamentId(snapshot.getKey());
+                    tournamentList.add(tournament);
+                }
+                tournamentAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle possible errors.
+            }
+        });
+    }
+
+
+
 }
